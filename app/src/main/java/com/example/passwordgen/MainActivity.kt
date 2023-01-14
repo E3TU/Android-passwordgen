c340d85fd01fe113dd7052f5ba815dd5d84c100b
E3TU <99178022+E3TU@users.noreply.github.com>
2023-01-14 18:29:05 +0200

    Changing colors and themes

--- app/src/main/java/com/example/passwordgen/MainActivity.kt
+++ app/src/main/java/com/example/passwordgen/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.passwordgen
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val actionBar = supportActionBar;
+        actionBar!!.title = "Password generator";
+        supportActionBar!!.setBackgroundDrawable(ColorDrawable(Color.parseColor("#2E84BC")));
     }
 }
