d11da6523b6e5fdeae1aa14747f9a2a1bd90df35
E3TU <99178022+E3TU@users.noreply.github.com>
2023-01-17 18:45:52 +0200

    Code for generating random passwords

--- app/src/main/java/com/example/passwordgen/MainActivity.kt
+++ app/src/main/java/com/example/passwordgen/MainActivity.kt
@@ -10,8 +10,10 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.LinearLayout
 import android.widget.SeekBar
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import com.google.android.material.internal.ViewUtils.hideKeyboard
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,10 +24,29 @@ class MainActivity : AppCompatActivity() {
         actionBar!!.title = "Password generator";
         supportActionBar!!.setBackgroundDrawable(ColorDrawable(Color.parseColor("#2E84BC")));
 
-        val passwordLength = findViewById<EditText>(R.id.passwordLength);
+        val passwordLength = findViewById<SeekBar>(R.id.passwordLength);
         val generateButton = findViewById<Button>(R.id.generateBtn);
+        val passwordOutput = findViewById<TextView>(R.id.passwordTxt);
+
+        fun generatepassWord(n: Int): String {
+            val chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%&/()=?+-_.,*";
+            val random = Random(System.nanoTime());
+            val password = StringBuilder();
+
+            for (i in 0 until n) {
+                val randomindex = random.nextInt(chars.length);
+                password.append(chars[randomindex]);
+            }
+            return password.toString();
+        }
+        fun printPassword() {
+            val n = 20;
+            //println(generatepassWord(n));
+            passwordOutput.text = generatepassWord(n);
+        }
 
         generateButton.setOnClickListener() {
+            printPassword();
             passwordLength.clearFocus();
         }
     }
