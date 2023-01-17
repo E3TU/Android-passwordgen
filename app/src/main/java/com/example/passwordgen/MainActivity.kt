23d0e2f398f79c76c611c54b2f9cddce4049ff87
E3TU <99178022+E3TU@users.noreply.github.com>
2023-01-17 19:37:13 +0200

    Password copy

--- app/src/main/java/com/example/passwordgen/MainActivity.kt
+++ app/src/main/java/com/example/passwordgen/MainActivity.kt
@@ -1,16 +1,15 @@
 package com.example.passwordgen
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.LinearLayout
-import android.widget.SeekBar
-import android.widget.TextView
+import android.widget.*
 import androidx.core.content.ContextCompat
 import com.google.android.material.internal.ViewUtils.hideKeyboard
 import kotlin.random.Random
@@ -27,6 +26,7 @@ class MainActivity : AppCompatActivity() {
         val passwordLength = findViewById<SeekBar>(R.id.passwordLength);
         val generateButton = findViewById<Button>(R.id.generateBtn);
         val passwordOutput = findViewById<TextView>(R.id.passwordTxt);
+        val copyButton = findViewById<Button>(R.id.copyBtn);
 
         fun generatepassWord(n: Int): String {
             val chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%&/()=?+-_.,*";
@@ -43,6 +43,15 @@ class MainActivity : AppCompatActivity() {
             val n = 20;
             //println(generatepassWord(n));
             passwordOutput.text = generatepassWord(n);
+        }
+
+        copyButton.setOnClickListener() {
+            val copyPassword = passwordOutput.text;
+            val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager;
+            val clipData = ClipData.newPlainText("text", copyPassword);
+            clipboardManager.setPrimaryClip(clipData);
+
+            Toast.makeText(this, "Password copied", Toast.LENGTH_LONG).show()
         }
 
         generateButton.setOnClickListener() {
