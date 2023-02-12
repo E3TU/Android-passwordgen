bd0872597cb4d7eb270914f1fec75459bff24764
E3TU <99178022+E3TU@users.noreply.github.com>
2023-02-12 23:24:43 +0200

    Radiobuttons

--- app/src/main/java/com/example/passwordgen/Settings.kt
+++ app/src/main/java/com/example/passwordgen/Settings.kt
@@ -4,6 +4,8 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore.Audio.Radio
+import android.widget.RadioButton
 
 class Settings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,12 +13,19 @@ class Settings : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val actionBar = supportActionBar
+        val switchtoDark = findViewById<RadioButton>(R.id.darkBtn)
+        val switchtoLight = findViewById<RadioButton>(R.id.lightBtn)
 
         actionBar!!.title = "Theme";
         actionBar.setBackgroundDrawable(ColorDrawable(Color.parseColor(actionbarColor)))
-
-
         actionBar.setDisplayHomeAsUpEnabled(true)
+
+        switchtoLight.setOnClickListener{
+            switchtoDark.isChecked = false
+        }
+        switchtoDark.setOnClickListener {
+            switchtoLight.isChecked = false
+        }
     }
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
